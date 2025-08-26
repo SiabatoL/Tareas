de72bac489d543d54890f55b5a57edca9a6d0c8f
SiabatoL <laurasiabato0@gmail.com>
2025-08-25 23:39:52 -0500

    prueba final

--- Entrega_1/python.py
+++ Entrega_1/python.py
@@ -7,7 +7,8 @@ sum=c[0]+d[1]
 res=d[0]+c[1]
 cruz=np.cross(c,d)
 punto=np.dot(d,c)
+divicion=np.divide(c,d)
 print("Valor de la suma",sum,"Valor de la resta",res)
 print("Valor producto punto",punto,"Valor producto cruz",cruz)
-
+print("Valor divicion",divicion)
 
